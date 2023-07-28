c2cdc35e9ed80b70dd45a1790fb297b301250e88
James LaFarr <lafarrjam99@gmail.com>
2023-07-28 14:20:12 -0400

    Added server.ts, which creates the server with the configuration laid out in app.ts.

---
+++ server/src/server.ts
@@ -0,0 +1,6 @@
+import app from "./app";
+
+app.listen(app.get("port"), () => {
+    console.log(`Server is running at http://localhost:${app.get("port")}`);
+    console.log("Press CTRL-C to stop");
+});
