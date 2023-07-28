394061f5759db7c06c0ef230a40e51604c9de715
James LaFarr <lafarrjam99@gmail.com>
2023-07-28 16:36:24 -0400

    Added global extension of the Request interface to allow adding custom properties on the Request object.

---
+++ server/src/types/express/index.d.ts
@@ -0,0 +1,12 @@
+import { JwtPayload } from "jsonwebtoken";
+
+// In order to add custom properties on the Request object
+declare global {
+    namespace Express {
+        export interface Request {
+            jwtPayload?: JwtPayload | string
+        }
+    }
+}
+
+export {}
