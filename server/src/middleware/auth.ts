3c014877e9957c365a72441a247c472519e5a890
James LaFarr <lafarrjam99@gmail.com>
2023-07-29 06:37:01 -0400

    Added ability to validate Google JWTs.

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -2,18 +2,29 @@ import { Request, Response, NextFunction } from "express";
 import { validateToken } from "../util/token";
 import { JwtPayload } from "jsonwebtoken";
 
+/**
+ * Determines if a given request contains a valid token, and either returns a response
+ * signifying that the request does not contain a valid token, or passes control to the
+ * next middleware in the middleware stack.
+ * 
+ * If the given request contains a valid token, the payload of the token will be stored as
+ * the jwtPayload prperty of the current Request object.
+ * 
+ * If the (valid) token's issuer is an external entity to the app (e.g. Google), and a user
+ * with the given email and issuer is not already present in the database, a new user will be 
+ * created with their email as well as denoting this account is associated with an account
+ * from the external issuer.
+ * 
+ * @param req The request object
+ * @param res the response object
+ * @param next the next function in the middleware stack
+ * @returns A response if the request does not contain a valid token, else returns nothing, and 
+ * passes control to the next middleware in the middleware stack
+ */
 export const isAuthenticated = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
-    const authHeader: string = req.headers.authorization;
-    if (!authHeader) {
-        return res
-            .status(400)
-            .json({
-                reason: "Request header does not contain JWT"
-            });
-    }
-    const token: string = authHeader.split(" ")[1];
+    const token: string = req.token;
     try {
-        const payload: JwtPayload | string = await validateToken(token);
+        const payload: JwtPayload | string = await validateToken(token, req.body.issuer);
         req.jwtPayload = payload;
         next();
     } catch (err: any) {
--- server/src/util/token.ts
+++ server/src/util/token.ts
@@ -1,5 +1,7 @@
 import { JwtPayload, sign, verify } from "jsonwebtoken";
 import { PRIVATE_KEY } from "./secrets";
+import { OAuth2Client, LoginTicket, TokenPayload } from "google-auth-library";
+import { GOOGLE_CLIENT_ID } from "../util/secrets";
 
 export const generateToken = (email: string): Promise<string> => {
     return new Promise((resolve, reject) => {
@@ -12,13 +14,27 @@ export const generateToken = (email: string): Promise<string> => {
     });
 };
 
-export const validateToken = (encodedToken: string): Promise<string | JwtPayload> => {
-    return new Promise((resolve, reject) => {
-       verify(encodedToken, PRIVATE_KEY, (err, payload) => {
-            if (err) {
-                reject();
+export const validateToken = (encodedToken: string, issuer: string): Promise<string | JwtPayload | TokenPayload> => {
+    return new Promise(async (resolve, reject) => {
+        if (issuer === "todo") {
+            verify(encodedToken, PRIVATE_KEY, (err, payload) => {
+                if (err) {
+                    reject();
+                }
+                resolve(payload);
+            });
+        } else {
+            const oauthClient = new OAuth2Client();
+            try {
+                const ticket: LoginTicket = await oauthClient.verifyIdToken({
+                    idToken: encodedToken,
+                    audience: GOOGLE_CLIENT_ID
+                });
+                const payload: TokenPayload = ticket.getPayload();
+                resolve(payload);
+            } catch (err: any) {
+                reject(err);
             }
-            resolve(payload);
-       });
+        }
     });
 };
