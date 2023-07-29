e645803a6b62ef9ea207bf9b0b68bd04ecd463e0
James LaFarr <lafarrjam99@gmail.com>
2023-07-29 06:45:56 -0400

    Added an export for the Google client ID of the application and for an enum containing the names of the application's OAuth providers.

--- server/src/util/secrets.ts
+++ server/src/util/secrets.ts
@@ -5,3 +5,9 @@ export const MONGO_URI = process.env.MONGO_URI;
 export const PRIVATE_KEY = process.env.JWT_PRIVATE_KEY;
 
 export const PORT = process.env.PORT;
+
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+
+export enum OAuthProviders {
+    GOOGLE
+}
