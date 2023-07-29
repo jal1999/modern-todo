9ec8e608c27bec628507cdc114d906ef70b9fb68
James LaFarr <lafarrjam99@gmail.com>
2023-07-29 06:44:40 -0400

    Removed unused import

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-app.listen(app.get("port"), () => {
+app.listen(app.get("port"), async () => {
     console.log(`Server is running at http://localhost:${app.get("port")}`);
     console.log("Press CTRL-C to stop");
 });
