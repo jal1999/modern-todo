8cc800c9471b80e08ed25b3ea66027274124e2a6
James LaFarr <lafarrjam99@gmail.com>
2023-07-29 16:57:17 -0400

    Fixed an async issue with mongoose.connect()

--- server/src/util/mongooseConnector.ts
+++ server/src/util/mongooseConnector.ts
@@ -2,5 +2,9 @@ import { connect } from "mongoose";
 import { MONGO_URI } from "./secrets";
 
 export const connectToDb = async (): Promise<void> => {
-    await connect(MONGO_URI);
+    try {
+        await connect(MONGO_URI);
+    } catch (err: any) {
+        console.log(err);
+    }
 };
