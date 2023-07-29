65f9d7891fd4cf9f790c4e5d150db0aae835e8f8
James LaFarr <lafarrjam99@gmail.com>
2023-07-29 18:12:54 -0400

    Added test to ensure that connectToDb(uri: string) indeed connects to the database.

--- server/tests/util.test.ts
+++ server/tests/util.test.ts
@@ -1,6 +1,20 @@
-import { describe } from "node:test";
 import { isValidEmailDomain } from "../src/util/email/generate";
-import { test, expect } from "@jest/globals";
+import { describe, test, expect } from "@jest/globals";
+import { MONGO_URI } from "../src/util/secrets";
+import { connectToDb } from "../src/util/mongooseConnector";
+import mongoose from "mongoose";
+
+describe("MongoDB Connection", () => {
+    test("Connects to the database with the proper URI", async () => {
+        const readyState: number = (await connectToDb(MONGO_URI)).connections[0].readyState;
+        expect(readyState).toBe(1);
+        await mongoose.connection.close();
+    });
+
+    test("Throws an error when trying to connect with the incorrect URI", async () => {
+        expect(typeof (await connectToDb("hello"))).not.toBe(typeof import("mongoose"));
+    });
+});
 
 describe("Email Validity", () => {
     test("Determines dslkaj is not a valid email domain", () => {
